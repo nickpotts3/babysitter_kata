05032d388e7be55b6269e03398df8976b3fe4c2f
Nicholas Potts <nicholaspotts@Nicholass-iMac.local>
2021-04-06 11:26:23 -0400

    Test validateEnd()

--- babysitter.py
+++ babysitter.py
@@ -1,4 +1,5 @@
 # WORKING HOURS 5PM-4AM 
+# CAN NOT START BEFORE 5 OR END AFTER 4 
 # REGULAR SHIFT START-TIME TO BEDTIME = $12/HOUR
 # BEDTIME SHIFT: BEDTIME-MIDNIGHT = $8/HOUR
 # MIDNIGHT SHIFT: MIDNIGHT - END-TIME = $16/HOUR
@@ -18,7 +19,7 @@ class Babysitter:
 
     # converts to military time
     def convertTime(self,time):
-        if (time <= 12 and time >= 5):
+        if ( time <= 12 and time >= 5):
             return int(time)
         elif(time == 0):
             return -1
@@ -27,17 +28,24 @@ class Babysitter:
 
     # ensure start is before endtime and not greater than 5pm. Allow babysitter to start AFTER bedtime
     def validateStart(self,start,end) -> bool:
-        if(start > 5):
+        if(self.convertTime(start) > 5):
             return False
-        elif(start > end):
+        elif(self.convertTime(start) > self.convertTime(end)):
             return False
-        elif(start == end):
+        elif(self.convertTime(start) == self.convertTime(end)):
             return False
         else:
             return True
 
-    def validateEnd(self,end) -> bool:
-        pass
+    def validateEnd(self, start, end) -> bool:
+        if(end > 16):
+            return False
+        elif(self.convertTime(start) > self.convertTime(end)):
+            return False
+        elif(self.convertTime(end) == self.convertTime(start)):
+            return False
+        else:
+            return True
 
     def validateBedtime(self,bedtime) -> bool:
         pass
@@ -74,7 +82,23 @@ negOne = sitter.convertTime(0)
 assert negOne == -1
 
 # ------- testing validateStart() ----------
-assert sitter.validateStart(5,10) == True # start at 5
+assert sitter.validateStart(5,10) == True # start at 5 
+
 assert sitter.validateStart(10,5) == False # end is before start
+
 assert sitter.validateStart(4,1) == False # ensuring can not start before 5
+
 assert sitter.validateStart(5,5) == False # start and end time can not be the same because pointless
+
+assert sitter.validateStart(5,4) == True # testing edge case 
+
+# -------- testing validateEnd() ----------
+assert sitter.validateEnd(5,10) == True # start at 5 end at 10 
+
+assert sitter.validateEnd(10,5) == False # end is before start
+
+assert sitter.validateEnd(4,1) == False # ensuring can not start before 5
+
+assert sitter.validateEnd(5,5) == False # start and end time can not be the same because pointless
+
+assert sitter.validateEnd(5,4) == True # edge case of earliest start time and latest end time 
